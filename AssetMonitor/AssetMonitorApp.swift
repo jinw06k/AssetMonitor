8045a6cfd66e1f9521e099c170c54e5165b3e059
Jin Wook Shin <49907866+jinw06k@users.noreply.github.com>
2026-01-22 15:53:20 -0500

    Initial Commit

---
+++ AssetMonitor/AssetMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetMonitorApp.swift
+//  AssetMonitor
+//
+//  Created by Jin Wook Shin on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct AssetMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
